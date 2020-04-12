568d72e1cffbf5bcee5ecee9c7ef5f1bc1fcfad3
AnduMekon <anduabe@yahoo.com>
2020-04-12 16:00:00 -0400

    chk6

--- src/app.js
+++ src/app.js
@@ -6,34 +6,108 @@ import './styles/styles.scss';
 
 
 
-class Car extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 0
-
-
-        };
-    }
-    handleAdd = () => {
-        this.setState((pre) => {
-            return {
-                count: pre.count + 1
-            }
-        });
-      }
-    render () {
-        return (
-            <div>
-            <p>count: {this.state.count}</p>
+class IndecisionApp extends React.Component {
+    render() {
+      const title = 'የምግብ ማኖ';
+      const subtitle = 'የሚፈልጉትን አየነት ምግብ ይምረጡ!';
+      const options = ['የመጀመሪያ ምርጫ', 'ሁለትኛ ምርጫ', 'ሶስተኛ ምርጫ'];
+  
+      return (
+        <div>
+          <Me />
+          <Header title={title} subtitle={subtitle} />
+          <Action />
+          <Options options={options} />
+          <AddOption />
+          <Me />
+          <MyForm />
           
-            <button
-                type="button"
-                onClick={this.handleAdd}>
-                Add
-            </button>
-            </div>
-        )
+        </div>
+      );
     }
-}
-ReactDOM.render(<Car />, document.getElementById('app'));
+  }
+  class MyForm extends React.Component {
+    render() {
+      return (
+        <form>
+          <h1>Hello</h1>
+          <p>Enter your name:</p>
+          <input style={{backgroundColor: "lightblue"}}
+            type="text"
+          />
+        </form>
+      );
+    }
+  }
+  class Me extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1> this is me</h1>
+          <h1 style={{backgroundColor: "lightblue"}}>የምግብ ማኖ!</h1>
+        </div>
+      );
+    }
+  }
+  class Header extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>{this.props.title}</h1>
+          <h2>{this.props.subtitle}</h2>
+        </div>
+      );
+    }
+  }
+  
+  class Action extends React.Component {
+  handlePick() {
+    alert ('እውይ ተጫኖኝ');
+  }
+  
+    render() {
+      return (
+        <div>
+          <button onClick={this.handlePick}>የቱን መምረጥ ይፈልጋሉ? test</button>
+        </div>
+      );
+    }
+  }
+  
+  class Options extends React.Component {
+    handleRemoveAll(){
+      alert('እርግጠኛ ኖዎት ማጥፋት ይፈልጋሉ? ')
+    }
+    render() {
+      return (
+        <div>
+        <button onClick={this.handleRemoveAll}>ለማጥፋት</button>
+          {
+            this.props.options.map((option) => <Option key={option} optionText={option} />)
+          }
+        </div>
+      );
+    }
+  }
+  
+  class Option extends React.Component {
+    render() {
+      return (
+        <div>
+        *  {this.props.optionText}
+        </div>
+      );
+    }
+  }
+  
+  class AddOption extends React.Component {
+    render() {
+      return (
+        <div>
+          AddOption component here
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
