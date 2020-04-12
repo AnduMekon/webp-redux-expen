4a134ce63de3c9dea7fade276cd7bb8e3989867c
AnduMekon <anduabe@yahoo.com>
2020-04-12 15:52:17 -0400

    chk-4

--- src/app.js
+++ src/app.js
@@ -5,6 +5,35 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+
+class Car extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
 
 
+        };
+    }
+    handleAdd = () => {
+        this.setState((pre) => {
+            return {
+                count: pre.count + 1
+            }
+        });
+      }
+    render () {
+        return (
+            <div>
+            <p>count: {this.state.count}</p>
+          
+            <button
+                type="button"
+                onClick={this.handleAdd}>
+                Add
+            </button>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<Car />, document.getElementById('app'));
