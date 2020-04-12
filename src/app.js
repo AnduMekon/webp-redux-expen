d9bad284d2ad06bd6df3cc6ec9e417d1129e4ce7
AnduMekon <anduabe@yahoo.com>
2020-04-12 14:47:16 -0400

    ck-1

--- src/app.js
+++ src/app.js
@@ -4,4 +4,7 @@ import TaskApp from './components/taskApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<TaskApp />, document.getElementById('app'));
+
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+
+
