8960f37f3f032dd450ca9e1bad429e769c063495
AnduMekon <anduabe@yahoo.com>
2020-04-12 20:29:57 -0400

    ck-1

--- src/app.js
+++ src/app.js
@@ -1,113 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+);
 
+const AddExpensePage = () => (
+  <div>
+    This is from my add expense component
+  </div>
+);
 
-class IndecisionApp extends React.Component {
-    render() {
-      const title = 'የምግብ ማኖ';
-      const subtitle = 'የሚፈልጉትን አየነት ምግብ ይምረጡ!';
-      const options = ['የመጀመሪያ ምርጫ', 'ሁለትኛ ምርጫ', 'ሶስተኛ ምርጫ'];
-  
-      return (
-        <div>
-          <Me />
-          <Header title={title} subtitle={subtitle} />
-          <Action />
-          <Options options={options} />
-          <AddOption />
-          <Me />
-          <MyForm />
-          
-        </div>
-      );
-    }
-  }
-  class MyForm extends React.Component {
-    render() {
-      return (
-        <form>
-          <h1>Hello</h1>
-          <p>Enter your name:</p>
-          <input style={{backgroundColor: "lightblue"}}
-            type="text"
-          />
-        </form>
-      );
-    }
-  }
-  class Me extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1> this is me</h1>
-          <h1 style={{backgroundColor: "lightblue"}}>የምግብ ማኖ!</h1>
-        </div>
-      );
-    }
-  }
-  class Header extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1>{this.props.title}</h1>
-          <h2>{this.props.subtitle}</h2>
-        </div>
-      );
-    }
-  }
-  
-  class Action extends React.Component {
-  handlePick() {
-    alert ('እውይ ተጫኖኝ');
-  }
-  
-    render() {
-      return (
-        <div>
-          <button onClick={this.handlePick}>የቱን መምረጥ ይፈልጋሉ? test</button>
-        </div>
-      );
-    }
-  }
-  
-  class Options extends React.Component {
-    handleRemoveAll(){
-      alert('እርግጠኛ ኖዎት ማጥፋት ይፈልጋሉ? ')
-    }
-    render() {
-      return (
-        <div>
-        <button onClick={this.handleRemoveAll}>ለማጥፋት</button>
-          {
-            this.props.options.map((option) => <Option key={option} optionText={option} />)
-          }
-        </div>
-      );
-    }
-  }
-  
-  class Option extends React.Component {
-    render() {
-      return (
-        <div>
-        *  {this.props.optionText}
-        </div>
-      );
-    }
-  }
-  
-  class AddOption extends React.Component {
-    render() {
-      return (
-        <div>
-          AddOption component here
-        </div>
-      );
-    }
-  }
-  
-  ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const EditExpensePage = () => (
+  <div>
+    This is from my edit expense component
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is from my help component
+  </div>
+);
+
+const NotFoundPage = () => (
+  <div>
+    404 - <Link to="/">Go home</Link>
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+  </header>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
